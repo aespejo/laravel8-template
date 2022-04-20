3823b4c0b32c2feaddd5288dac5a2bd934b43852
Alvin Espejo <espejo.a2@gmail.com>
2022-04-20 22:40:12 +0800

    Added command to fetch ,env file from aws parameter store.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,12 @@ npm install
 npm run production
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
-# TODO: Generate a .env file
+# Get .env file from AWS SSM
+aws --region ap-southeast-1 ssm get-parameter \
+    --with-decryption \
+    --name /cloudcasts/staging/env \
+    --output text \
+    --query 'Parameter.Value' > .env 
  
 
 # Create our build artifact
@@ -17,3 +22,23 @@ zip -qur builds/cloudcasts.zip .env # Grab our testing .env file for now
 # Upload artifact to s3
 # variable $1 is the command argument that was added when calling this file
 aws s3 cp builds/cloudcasts.zip s3://cloudcasts-$1-artifacts/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
+
+# Change rights to the build.sh file to avoid permission denied error on build
+# git update-index --add --chmod=+x build.sh
+# git commit -m 'Make build.sh executable'
+# git push
+
+# OR
+# before_install:
+#   - chmod +x build.sh
+
+# Command to save .env file to AWS SSM 
+
+# Linux CLI
+# aws --profile aespejo --region ap-southeast-1 ssm put-parameter \
+#     --name /cloudcasts/staging/env \
+#     --type SecureString \
+#     --value file://.env
+
+# Windows powershell cli
+# aws --profile aespejo --region ap-southeast-1 ssm put-parameter --name /cloudcasts/staging/env --type SecureString --value file://.env
