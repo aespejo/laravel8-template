0d5b254dc67e8ac190536cb913d202dec1e427d5
Alvin Espejo <espejo.a2@gmail.com>
2022-04-20 22:05:06 +0800

    update file right

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,4 +16,4 @@ zip -qur builds/cloudcasts.zip .env # Grab our testing .env file for now
 
 # Upload artifact to s3
 # variable $1 is the command argument that was added when calling this file
-# aws s3 cp builds/cloudcasts.zip s3://cloudcasts-$1-artifacts/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
+aws s3 cp builds/cloudcasts.zip s3://cloudcasts-$1-artifacts/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
