eaa47a3140072d6b5ea66029312495d945c09a66
Alvin Espejo <espejo.a2@gmail.com>
2022-04-24 03:42:33 +0800

    update deploy script bucket source for the build artifacts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,11 +8,11 @@ aws --region ap-southeast-1 deploy create-deployment \
     --application-name cloudcasts-$1-deploy-app \
     --deployment-group-name "cloudcasts-$1-http-deploy-group" \
     --description "Deploying tag $CODEBUILD_WEBHOOK_TRIGGER" \
-    --s3-location "bucket=cloudcasts-$1-artifacts,bundleType=zip,key=$CODEBUILD_RESOLVED_SOURCE_VERSION.zip"
+    --s3-location "bucket=cloudcasts-artifacts-codebuild,bundleType=zip,key=$CODEBUILD_RESOLVED_SOURCE_VERSION.zip"
 
 # Deploy queues
 aws --region ap-southeast-1 deploy create-deployment \
     --application-name cloudcasts-$1-deploy-app \
     --deployment-group-name "cloudcasts-$1-queue-deploy-group" \
     --description "Deploying tag $CODEBUILD_WEBHOOK_TRIGGER" \
-    --s3-location "bucket=cloudcasts-$1-artifacts,bundleType=zip,key=$CODEBUILD_RESOLVED_SOURCE_VERSION.zip"
+    --s3-location "bucket=cloudcasts-artifacts-codebuild,bundleType=zip,key=$CODEBUILD_RESOLVED_SOURCE_VERSION.zip"
