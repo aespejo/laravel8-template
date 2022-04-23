c71d78c80dc67f291dce1aa35a1f92aedf0c3382
Alvin Espejo <espejo.a2@gmail.com>
2022-04-24 03:38:31 +0800

    update s3 bucket source for the artifacts

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,7 @@ zip -qur builds/cloudcasts.zip .env # Grab our testing .env file for now
 
 # Upload artifact to s3
 # variable $1 is the command argument that was added when calling this file
-aws s3 cp builds/cloudcasts.zip s3://cloudcasts-artifacts/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
+aws s3 cp builds/cloudcasts.zip s3://cloudcasts-artifacts-codebuild/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
 
 # Change rights to the build.sh file to avoid permission denied error on build
 # git update-index --add --chmod=+x build.sh
