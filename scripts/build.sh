e7894b8865bc242ad5a643dbc865ed737f5d59c8
Alvin Espejo <espejo.a2@gmail.com>
2022-04-24 04:15:55 +0800

    updated build and deploy script to trigger only on specific banch or tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-# Production assets/dependencies
-npm install
-npm run production
-composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev 
+DO_BUILD="no"
 
-# Create our build artifact
-git archive -o builds/cloudcasts.zip --worktree-attributes HEAD
-zip -qur builds/cloudcasts.zip vendor
-zip -qur builds/cloudcasts.zip public
-zip -qur builds/cloudcasts.zip .env # Grab our testing .env file for now
+if [[ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/main" ]]; then
+    DO_BUILD="yes"
+fi
 
-# Upload artifact to s3
-# variable $1 is the command argument that was added when calling this file
-aws s3 cp builds/cloudcasts.zip s3://cloudcasts-artifacts-codebuild/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
+if [[ "$CODEBUILD_WEBHOOK_TRIGGER" == "tag/"* ]]; then
+    DO_BUILD="yes"
+fi
+
+if [[ "$DO_BUILD" == "yes" ]]; then
+    # Production assets/dependencies
+    npm install
+    npm run production
+    composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev 
+
+    # Create our build artifact
+    git archive -o builds/cloudcasts.zip --worktree-attributes HEAD
+    zip -qur builds/cloudcasts.zip vendor
+    zip -qur builds/cloudcasts.zip public
+    zip -qur builds/cloudcasts.zip .env # Grab our testing .env file for now
+
+    # Upload artifact to s3
+    # variable $1 is the command argument that was added when calling this file
+    aws s3 cp builds/cloudcasts.zip s3://cloudcasts-artifacts-codebuild/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip
+fi
 
 # Change rights to the build.sh file to avoid permission denied error on build
 # git update-index --add --chmod=+x build.sh
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,16 +3,28 @@
 # AWS CLI
 # https://docs.aws.amazon.com/cli/latest/reference/deploy/create-deployment.html
 
-# Deploy web servers
-aws --region ap-southeast-1 deploy create-deployment \
-    --application-name cloudcasts-$1-deploy-app \
-    --deployment-group-name "cloudcasts-$1-http-deploy-group" \
-    --description "Deploying tag $CODEBUILD_WEBHOOK_TRIGGER" \
-    --s3-location "bucket=cloudcasts-artifacts-codebuild,bundleType=zip,key=$CODEBUILD_RESOLVED_SOURCE_VERSION.zip"
+DEPLOY_ENV=""
 
-# Deploy queues
-aws --region ap-southeast-1 deploy create-deployment \
-    --application-name cloudcasts-$1-deploy-app \
-    --deployment-group-name "cloudcasts-$1-queue-deploy-group" \
-    --description "Deploying tag $CODEBUILD_WEBHOOK_TRIGGER" \
-    --s3-location "bucket=cloudcasts-artifacts-codebuild,bundleType=zip,key=$CODEBUILD_RESOLVED_SOURCE_VERSION.zip"
+if [[ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/main" ]]; then
+    DEPLOY_ENV="staging"
+fi
+
+if [[ "$CODEBUILD_WEBHOOK_TRIGGER" == "tag/"* ]]; then
+    DEPLOY_ENV="production"
+fi
+
+if [[ "$DEPLOY_ENV" != "" ]]; then
+    # Deploy web servers
+    aws --region ap-southeast-1 deploy create-deployment \
+        --application-name cloudcasts-$DEPLOY_ENV-deploy-app \
+        --deployment-group-name "cloudcasts-$DEPLOY_ENV-http-deploy-group" \
+        --description "Deploying tag $CODEBUILD_WEBHOOK_TRIGGER" \
+        --s3-location "bucket=cloudcasts-artifacts-codebuild,bundleType=zip,key=$CODEBUILD_RESOLVED_SOURCE_VERSION.zip"
+
+    # Deploy queues
+    aws --region ap-southeast-1 deploy create-deployment \
+        --application-name cloudcasts-$DEPLOY_ENV-deploy-app \
+        --deployment-group-name "cloudcasts-$DEPLOY_ENV-queue-deploy-group" \
+        --description "Deploying tag $CODEBUILD_WEBHOOK_TRIGGER" \
+        --s3-location "bucket=cloudcasts-artifacts-codebuild,bundleType=zip,key=$CODEBUILD_RESOLVED_SOURCE_VERSION.zip"
+fi
