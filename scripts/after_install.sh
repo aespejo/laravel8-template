7b8201f4bfbc6dcaca88056dbd2fa21843760608
Alvin Espejo <espejo.a2@gmail.com>
2022-04-30 00:34:41 +0800

    added script debug code

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -17,11 +17,12 @@ cd /var/www/alvinespejo.com
 echo -- /alvinespejo/$DEPLOY_ENV/env
 
 aws --region ap-southeast-1 ssm get-parameter --with-decryption --name /alvinespejo/$DEPLOY_ENV/env --output text --query 'Parameter.Value' > .env 
-
+echo "pull prod env"
 # Set permissions
 sudo chown -R ubuntu:www-data /var/www/alvinespejo.com
 sudo chmod -R 775 /var/www/alvinespejo.com/storage
 sudo chmod -R 775 /var/www/alvinespejo.com/bootstrap/cache
+echo "Set permissions"
 
 # Below conditional syntax from here:
 # https://stackoverflow.com/questions/229551/how-to-check-if-a-string-contains-a-substring-in-bash
@@ -31,11 +32,16 @@ sudo chmod -R 775 /var/www/alvinespejo.com/bootstrap/cache
 # Reload php-fpm (clear opcache) if a web server
 if [[ "$DEPLOYMENT_GROUP_NAME" == *"http"* ]]; then
     service php8.0-fpm reload
+    echo "Restart PHP"
 fi
 
 # Start supervisor jobs (if supervisor is used)
 if [[ "$DEPLOYMENT_GROUP_NAME" == *"queue"* ]]; then
     supervisorctl start all
+    echo "Restart supervisor"
 fi
 
+
 sudo -u ubuntu php artisan migrate --force
+
+echo "DB migration"
